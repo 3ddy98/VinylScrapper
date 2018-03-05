97554a0b4b131285c6d40f0cba8568913864fcb4
3ddy98 <eaponce.vanguard@gmail.com>
2018-03-04 16:27:57 -0800

    [+] Small changes to text shown on terminal window

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-#!usr/bin/python
-
 import time
 from bs4 import BeautifulSoup as bs
 import requests
+import time
+import csv
 
 def main():
+
+    print('Please wait 3 seconds while we warm things up...\n')#This is meant to keep the script from being blocked on website
+    time.sleep(3)
+
     #link = input('Please enter Vinyl Profile Link: ')
     link = 'https://www.discogs.com/Dexys-Midnight-Runners-Emerald-Express-Come-On-Eileen/release/3151574'
     page = requests.get(link) #requests the webpage
     soup = bs(page.content,'lxml') #parses the website with lxml framerwork
     name_container = soup.find('meta',itemprop='name') #finds meta container with name of artist and vinyl
+
     full_title = name_container['content']
     title_and_songname = full_title.rsplit('-')
-    title = title_and_songname[0]
-    song_name = title_and_songname[1].replace(' ','',1)
+    artist = title_and_songname[0]
+    vinyl = title_and_songname[1].replace(' ','',1)
 
+    print('Artist: ', artist)
+    print('Song  : ', vinyl)
+    print('This will close in 10 seconds')
+    print('You should now have results gathered in your excel file')
+    time.sleep(10)
+
+def csv_export(artist, vinyl, link,):
+    print('something')
 
 
 if __name__ == "__main__":
