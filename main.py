8059ee6881d8adc495857d96d318fd001cebef86
3ddy98 <eponc006@ucr.edu>
2018-04-18 16:03:14 -0700

    [+] Added Name seeking code, Artist name is now foundX

--- main.py
+++ main.py
@@ -13,9 +13,18 @@ def main():
     get_info(link)
 
 def get_info(link):
-    page_load = requests.get(link) #getting the page to load with request modul
+    page_load = requests.get(link) #getting the page to load with request module
     p = page_load.content #short variable as a name for content on page
-    soup = bs (p, 'html.parser')
+    soup = bs (p, 'html5lib')#parses content using the python html parser
+
+    #Starting the search for needed information
+    profile_container = soup.select(".profile")  #finding the profile container on the page
+    for results in profile_container:
+        artist_title_container = results.find_all("h1")
+        for results in artist_title_container:
+            artist = results.find('a').string
+            print('\n\n' + artist)
+
 
 if __name__ == "__main__":
     main()
